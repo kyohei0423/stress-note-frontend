a5ca5b9aea805d85b0e16905cb76ef79622f94bb
Kyohei Seo <kyo199333@gmail.com>
2020-08-26 19:15:28 +0900

    Add link to sign upage and log in page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Index() {
   return (
     <div>
       <h1>Top</h1>
+      <ul>
+        <li>
+          <Link href="/signup">
+            <a>ユーザー登録</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/login">
+            <a>ログイン</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
