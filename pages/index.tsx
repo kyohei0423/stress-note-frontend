145cb0990e221f1cc8f7fd7ec312444763bca784
Kyohei Seo <kyo199333@gmail.com>
2020-08-26 19:56:21 +0900

    Format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 function Index() {
   return (
-    <div>
-      <h1>Top</h1>
-      <ul>
-        <li>
-          <Link href="/signup">
-            <a>ユーザー登録</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <a>ログイン</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  )
+    <>
+      <div>
+        <h1>Top</h1>
+        <ul>
+          <li>
+            <Link href="/signup">
+              <a>ユーザー登録</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">
+              <a>ログイン</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default Index
+export default Index;
