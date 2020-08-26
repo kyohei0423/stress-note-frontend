f404656b03946d48d16bb65f9a516a26d2ef417c
Kyohei Seo <kyo199333@gmail.com>
2020-08-26 22:02:32 +0900

    Add layout component

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+function Layout({ children, subTitle }) {
+  return (
+    <>
+      <Head>
+        <title>StressNote | {subTitle}</title>
+      </Head>
+      <div>{children}</div>
+    </>
+  );
+}
+
+export default Layout;
