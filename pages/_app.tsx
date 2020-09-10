b8043c3066df17652f6fc3faa01361a6b01bcfcf
Kyohei Seo <kyo199333@gmail.com>
2020-09-10 22:32:48 +0900

    Add provider to connect redux store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
+import { Provider } from "react-redux";
+
+import store from "@redux/store";
+
 import "styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
