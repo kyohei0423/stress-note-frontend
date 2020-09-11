f6d90805cfa4e0bc05eca35fbc316c2943c5ae45
Kyohei Seo <kyo199333@gmail.com>
2020-09-12 00:10:07 +0900

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,7 @@ module.exports = {
     "react/jsx-filename-extension": [2, { extensions: [".tsx"] }], // tsxファイルを対象にする
     "react/react-in-jsx-scope": 0, // Next.jsでは、自動でReactをimportするので無効にする
     "@typescript-eslint/explicit-module-boundary-types": 0, // 型推論を使用するため明示的に戻り地の型を指定しなくても良い
+    "jsx-a11y/anchor-is-valid": 0, // Next.jsではスタイリングのためにhrefのないaタグを使用することがあるので無効にする
+    "import/prefer-default-export": 0, // named exportを使用するため無効にする
   },
 };
