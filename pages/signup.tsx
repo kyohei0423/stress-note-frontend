3a63a5d512396117eb3de81a0be2cc03595cb076
Kyohei Seo <kyo199333@gmail.com>
2020-09-23 12:08:22 +0900

    Add form with react-hook-form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,38 @@
+import Layout from "@atoms/layout";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
 function Signup() {
+  const { register, handleSubmit, errors } = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
+  };
+
   return (
-    <div>
+    <Layout subTitle="新規登録">
       <h1>Sign up</h1>
-    </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input type="email" name="email" ref={register({ required: true })} />
+          {errors.email && <span>メールアドレスは必須です</span>}
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            ref={register({ required: true })}
+          />
+          {errors.password && <span>パスワードは必須です</span>}
+        </div>
+        <div>
+          <input type="submit" value="新規登録する" />
+        </div>
+      </form>
+    </Layout>
   );
 }
 
