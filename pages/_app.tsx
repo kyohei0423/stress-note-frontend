ada2b5a036190dbaba81b4634017a35a5030caa4
Kyohei Seo <kyo199333@gmail.com>
2021-03-08 22:29:06 +0900

    Remove codes related to redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
-import { Provider } from "react-redux";
 import { AppProps } from "next/app";
-
-import store from "@redux/store";
 
 import "styles/ress.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Provider>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Component {...pageProps} />
   );
 }
--- redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import { authenticationReducer } from "@redux/authentication/authenticationSlice";
-
-export default configureStore({
-  reducer: {
-    authentication: authenticationReducer,
-  },
-});
